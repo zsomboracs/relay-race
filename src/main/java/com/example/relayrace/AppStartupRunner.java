47fe69e2aea1ee42da5bd4d82915bb3e555a5e37
Zsombor Acs <Zsombor_Acs@epam.com>
2021-12-09 11:20:22 +0100

    Basic Spring Boot skeleton with ApplicationRunner

---
+++ src/main/java/com/example/relayrace/AppStartupRunner.java
@@ -0,0 +1,13 @@
+package com.example.relayrace;
+
+import com.example.relayrace.service.RaceSimulationService;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class AppStartupRunner implements ApplicationRunner {
+
+}
